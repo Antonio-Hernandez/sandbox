831f798586f4de22cae2f4d5a341f067e11f0302
Antonio Hernandez <antonio.hernandez@my.jcu.edu.au>
2017-08-14 16:06:26 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """Antonio Hernandez"""
+
+username = input("Please enter a username: ")
+while username == "":
+    username = input("Invalid Username. Please enter a valid username: ")
+
+print("hello {}".format(username))
