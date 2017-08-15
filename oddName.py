3b17fb0f42c70d3a871d11ef951c147cab8a2242
Antonio-Hernandez <antonio.hernandez@my.jcu.edu.au>
2017-08-15 15:55:57 +1000

    add step format

--- oddName.py
+++ oddName.py
@@ -4,4 +4,4 @@ username = input("Please enter a username: ")
 while username == "":
     username = input("Invalid Username. Please enter a valid username: ")
 
-print("hello {}".format(username))
+print("hello {}".format(username[::2]))
